4db4b48c2ab2201f89739d1844a190b15b36ff55
NguyenAnhDuy98 <anhduy1115@gmail.com>
2018-11-21 20:38:40 +0700

    Chinh sua login

--- Login.cs
+++ Login.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace QLNVApp
 {
@@ -19,7 +21,7 @@ namespace QLNVApp
 
         private void btLogin_Click(object sender, EventArgs e)
         {
-            string userName = txtUserName.Text;
+            string userName = txtUserName.Text.Trim();
             string passWord = txtPassWord.Text;
             if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(passWord))
             {
@@ -27,8 +29,8 @@ namespace QLNVApp
             }
             else
             {
-                if (userName == "admin" && passWord == "123456")
-                //if (Login(userName, passWord) == true)
+                //if (userName == "admin" && passWord == "123456")
+                if (Login(userName, passWord) == true)
                 {
                     this.DialogResult = DialogResult.OK;
                     this.Close();
@@ -47,22 +49,27 @@ namespace QLNVApp
                 }
             }
         }
-        //private bool Login(string username, string password)
-        //{
-        //    string cnStr = "Server = .; Database = CoffeeShop; Integrated security = true;";
-        //    SqlConnection cn = new SqlConnection(cnStr);
-        //    cn.Open();
-        //    string sql = "SELECT COUNT(UserName) FROM Users WHERE UserName = '" + username + "' AND Password = '" + password + "'";
-        //    SqlCommand cmd = new SqlCommand();
-        //    cmd.Connection = cn;
-        //    cmd.CommandText = sql;
-        //    cmd.CommandType = CommandType.Text;
-        //    int count = (int)cmd.ExecuteScalar();
-        //    cn.Close();
-        //    if (count == 1)
-        //        return true;
-        //    else
-        //        return false;
-        //}
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private bool Login(string username, string password)
+        {
+            string cnStr = "Data Source=DESKTOP-67RKQE5\\SQLEXPRESS;Initial Catalog=QLQuanCafe;Integrated Security=True";
+            SqlConnection cn = new SqlConnection(cnStr);
+            cn.Open();
+            string sql = "SELECT COUNT(Account) FROM TaiKhoan WHERE Account = '" + username + "' AND Password = '" + password + "'";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            int count = (int)cmd.ExecuteScalar();
+            cn.Close();
+            if (count == 1)
+                return true;
+            else
+                return false;
+        }
     }
 }
