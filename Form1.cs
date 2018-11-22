3a71362e55eaabb9919fa51d1f32be8e6f669587
quan hieu <quanhieu18898@gmail.com>
2018-11-22 10:23:50 +0700

    them ev click menu

--- Form1.cs
+++ Form1.cs
@@ -57,5 +57,17 @@ namespace QLNVApp
         {
             Application.Exit();
         }
+
+        private void doanhThuToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            frmQLDoanhThu frm = new frmQLDoanhThu();
+            frm.ShowDialog();
+        }
+
+        private void nguyênLiệuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmQLKho frm = new frmQLKho();
+            frm.ShowDialog();
+        }
     }
 }
